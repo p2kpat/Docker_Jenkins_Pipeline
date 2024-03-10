18cff9137882265fc9b3d1ab374c9de60ba36fe0
“pat” <priyankkp@gmail.com>
2024-03-10 02:57:08 +0000

    adding application files and compilation of file

---
+++ myJavaapp.java
@@ -0,0 +1,11 @@
+public class myJavaapp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, this is a custom java application to print this sentence..");
+
+}
+
+}
+
+
