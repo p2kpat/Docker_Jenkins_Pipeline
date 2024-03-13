6a2a493a510b170768bf8b2940e72144b96dd6ee
“pat” <priyankkp@gmail.com>
2024-03-13 02:58:50 +0000

    update the java application V1.2

--- myJavaapp.java
+++ myJavaapp.java
@@ -3,8 +3,9 @@ public class myJavaapp {
     public static void main(String[] args) {
 
         System.out.println("Hello, this is a custom java application to print this sentence..");
-
-}
+	System.out.println("Should I write a book?");
+	System.out.println("Hello, I could write a book like this!..");
+    }
 
 }
 
